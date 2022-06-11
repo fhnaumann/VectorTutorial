73536821388fed41287b21b5f192daf57cc8bb56
Felix Naumann <felix.naumann@student.uni-luebeck.de>
2022-06-11 22:49:01 +0200

    m

--- introduction/src/introduction.py
+++ introduction/src/introduction.py
@@ -1,6 +1,9 @@
+from distutils.command.build_scripts import first_line_re
+from telnetlib import DO
 from tkinter import TOP
 from manim import *
 import time
+import math
 
 class SimpleGraph(VectorScene):
 	def construct(self):
@@ -35,7 +38,7 @@ class SimpleGraphWithFields(VectorScene):
 		self.add(Square(side_length=1)
 			.set_color(ORANGE)
 			.shift(
-				axes.c2p(*(RIGHT*vec.get_x()*1.675)), 
+				axes.c2p(*(RIGHT*vec.get_x()*2.34)), 
 				axes.c2p(*(UP*vec.get_y()*2.5))
 			)
 		)
@@ -75,13 +78,7 @@ class Simple3DGraph(ThreeDScene):
 
 class UnderstandOrigin(VectorScene):
 	def construct(self):
-		axes = Axes(x_range=[-6, 6], x_length=12, y_range=[-6, 6], y_length=12)
-		axes.add_coordinates()
-		self.add(
-			axes.get_x_axis().set_color(RED), 
-			axes.get_y_axis().set_color(BLUE), 
-			axes.get_x_axis_label(label="x").set_color(RED), 
-			axes.get_y_axis_label(label="z").set_color(BLUE).shift(UP*2.5))
+		axes = setup_2d_axes(self)
 		playerLoc = self.add_vector([3, 2], animate=False).set_color(PURPLE)
 		player_loc_label = self.label_vector(vector=playerLoc, label=MathTex(r"loc=\begin{bmatrix}3 \\ 2\end{bmatrix}", color=PURPLE), animate=False)
 		playerDir = self.add_vector([0, 1], animate=False).set_color(ORANGE).shift(axes.c2p(playerLoc.get_x()*2, playerLoc.get_y()*2))
@@ -101,6 +98,144 @@ class UnderstandOrigin(VectorScene):
 		self.play(lookingFor.animate.shift(axes.c2p(playerLoc.get_x()*2, playerLoc.get_y()*2)))
 		self.play(FadeIn(line2))
 		
+
+class Addition(VectorScene):
+	def construct(self):
+		# self.simple_addition()
+		self.triple_addition()
+	
+	def triple_addition(self):
+		axes = setup_2d_axes(self)
+		vec1 = self.add_vector([3,1], animate=False).set_color(GREEN)
+		vec2 = self.add_vector([-1,-2], animate=False).set_color(ORANGE)
+		vec3 = self.add_vector([-3,4], animate=False).set_color(YELLOW)
+		self.wait()
+		self.play(vec2.animate.move_to(axes.c2p(*(vec1.get_end() + vec2.get_end()/2))))
+		self.wait()
+		self.play(vec3.animate.move_to(axes.c2p(*(vec2.get_end() + vec3.get_end()/2))))
+		self.wait()
+		self.add_vector(vec3.get_end(), animate=True, color=PURPLE)
+		self.wait(2)
+	
+	def simple_addition(self):
+		axes = setup_2d_axes(self)
+		vec1 = self.add_vector([3,1], animate=False).set_color(GREEN)
+		vec2 = self.add_vector([2,3], animate=False).set_color(ORANGE)
+		self.wait()
+		self.play(vec2.animate.move_to(axes.c2p(*(vec1.get_end() + vec2.get_end()/2))))
+		self.wait()
+		vec3 = self.add_vector(vec2.get_end(), animate=True, color=PURPLE)
+		self.wait(2)
+
+class Subtraction(VectorScene):
+	def construct(self):
+		self.simple_subtraction()
+
+	def simple_subtraction(self):
+		axes = setup_2d_axes(self)
+		vec1 = self.add_vector([3,1], animate=False).set_color(GREEN)
+		vec2 = self.add_vector([2,3], animate=False).set_color(ORANGE)
+		self.wait()
+		self.play(Transform(vec2, Vector([-2,-3]).set_color(ORANGE)))
+		self.wait()
+		self.play(vec2.animate.move_to(axes.c2p(*(vec1.get_end() + vec2.get_end()/2))))
+		self.wait()
+		self.add_vector(vec2.get_end(), animate=True, color=PURPLE)
+		self.wait(2)
+
+class DotProduct(VectorScene):
+	def construct(self):
+		# self.simple_dot_product()
+		self.show_dot_product_values()
+
+	def show_dot_product_values(self):
+		axes = Axes(x_range=[-2,2], x_length=12, y_range=[-2,2], y_length=12)
+		axes.add_coordinates()
+		self.add(
+			axes.get_x_axis().set_color(RED), 
+			axes.get_y_axis().set_color(BLUE), 
+			axes.get_x_axis_label(label="x").set_color(RED), 
+			axes.get_y_axis_label(label="z").set_color(BLUE).shift(UP*2.5)
+		)
+		fixed_coords = axes.c2p(*[1/2,math.sqrt(3)/2])
+		fixed = self.add_vector(fixed_coords, animate=False)
+		rotating = fixed.copy().rotate(0.1*DEGREES)
+		self.add(rotating)
+		rotating_ref = rotating.copy()
+		theta_tracker = ValueTracker(0)
+		angle = Angle(fixed, rotating, other_angle=True)
+		self.add(fixed, rotating, angle)
+
+		# rotating.add_updater(
+		# 	lambda x: x.become(rotating_ref.copy()).rotate(
+		# 		theta_tracker.get_value() * DEGREES, about_point=ORIGIN
+		# 	)
+		# )
+		# angle.add_updater(
+		# 	lambda x: x.become(Angle(fixed, rotating, other_angle=theta_tracker.get_value()>180))
+		# )
+		
+		self.play(theta_tracker.animate.set_value(90), run_time=1)
+		self.wait()
+		self.play(theta_tracker.animate.set_value(180), run_time=1)
+		self.wait()
+		self.play(theta_tracker.animate.set_value(270), run_time=1)
+		self.wait()
+		self.play(theta_tracker.animate.set_value(360), run_time=1)
+
+	def simple_dot_product(self):
+		axes = setup_2d_axes(self)
+		onto = self.add_vector([4,1], animate=False).set_color(GREEN)
+		self.label_vector(onto, label=MathTex(r"\vec{a}", color=GREEN), animate=False)
+		squashing = self.add_vector([1,2], animate=False).set_color(ORANGE)
+		self.label_vector(squashing, label=MathTex(r"\vec{b}", color=ORANGE), animate=False)
+		self.wait()
+		dot_product = np.dot([1,2], [4,1])
+		projection_length = dot_product/np.linalg.norm([4,1])
+		percent = projection_length/np.linalg.norm([4,1])
+		ending = axes.c2p(4*percent, 1*percent, 0)
+		dotted_line = DashedLine(start=[1,2,0], end=ending).set_color(GRAY)
+		line_on_origin = np.array([ending[0]-1, ending[1]-2, 0]) # represents the dotted_line from the origin (not drawn)
+		ending_vector = Vector(ending).set_color(PURPLE)
+		self.play(Transform(squashing.copy(), ending_vector), Write(MathTex(r"\vec{b_{a}}").set_color(PURPLE).next_to(ending_vector, direction=DOWN*0.15)), Create(dotted_line), run_time=3)
+
+		rotation = math.acos(np.dot([4,1], [5,0])/(np.linalg.norm([4,1])*np.linalg.norm([5,0])))
+		angle = Arc(radius=0.3, angle=PI/2, arc_center=ending).set_color(GRAY).rotate(angle=rotation, about_point=ending)
+
+		self.play(Create(angle), Create(Dot([ending[0] + 0.08, ending[1] + 0.15, 0], radius=0.02).set_color(GRAY)))
+
+		
+		first_brace = BraceBetweenPoints(point_1=ORIGIN, point_2=ending).set_color(PURPLE).shift(line_on_origin*0.5)
+		first_length = MathTex(r"\lvert\lvert\vec{b_{a}}\right\rVert", color=PURPLE).next_to(first_brace, direction=DOWN*0.4)
+		self.play(FadeIn(first_brace), Write(first_length))
+		
+		print(line_on_origin)
+		second_brace = BraceBetweenPoints(point_1=ORIGIN, point_2=[4,1,0]).set_color(GREEN).shift(line_on_origin*1.2)
+		second_length = MathTex(r"\lvert\lvert\vec{a}\right\rVert", color=GREEN).next_to(second_brace, direction=DOWN*0.4)
+		self.play(FadeIn(second_brace), Write(second_length))
+
+		equation = MathTex(r"\vec{a}", r"\bullet", r"\vec{b}", r"=", r"\lvert\lvert", r"\vec{b_{a}}", r"\right\rVert", r"\cdot", r"\lvert\lvert", r"\vec{a}", r"\rvert\rvert").scale(1.5).shift(DOWN*4.5, RIGHT*3.5)
+		
+		equation = MathTex(r"\vec{a}", r"\bullet", r"\vec{b}", r"=", r"\lvert\lvert\vec{b_{a}}\right\rVert", r"\cdot", r"\lvert\lvert\vec{a}\rvert\rvert").scale(1.5).shift(DOWN*4.5, RIGHT*3.5)
+		
+		
+		equation.set_color_by_tex(tex=r"\vec{a}", color=GREEN)
+		equation.set_color_by_tex(tex=r"\lvert\lvert\vec{a}\rvert\rvert", color=GREEN)
+		equation.set_color_by_tex(tex=r"\vec{b}", color=ORANGE)
+		equation.set_color_by_tex(tex=r"\vec{b_{a}}", color=PURPLE)
+		equation.set_color_by_tex(tex=r"\lvert\lvert\vec{b_{a}}\right\rVert", color=PURPLE)
+		self.play(Write(equation))
+		self.wait()
+
+def setup_2d_axes(scene):
+	axes = Axes(x_range=[-6, 6], x_length=12, y_range=[-6, 6], y_length=12)
+	axes.add_coordinates()
+	scene.add(
+		axes.get_x_axis().set_color(RED), 
+		axes.get_y_axis().set_color(BLUE), 
+		axes.get_x_axis_label(label="x").set_color(RED), 
+		axes.get_y_axis_label(label="z").set_color(BLUE).shift(UP*2.5))
+	return axes
 
 def fromMC(vector):
 	return [vector[2], vector[0], vector[1]]
--- util/src/main/java/io/github/wand555/Main.java
+++ util/src/main/java/io/github/wand555/Main.java
@@ -2,9 +2,12 @@ package io.github.wand555;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
+
+import java.util.stream.Stream;
 
 public class Main extends JavaPlugin {
 
@@ -12,7 +15,9 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         Player player = Bukkit.getPlayer("wand555");
         //DisplayUtil.showXZPlaneRepeat(new Vector(0d, player.getLocation().getY(), 0d), 5, Color.RED, player.getWorld(), this);
-        DisplayUtil.showBasisVectorRepeat(player.getLocation().toVector(), 5, 15, true, player.getWorld(), this);
-        DisplayUtil.showVectorRepeat(new Vector(4, 2, 4), player.getLocation().toVector(), 10, Color.PURPLE, true, player.getWorld(), this);
+        DisplayUtil.showBasisVectorRepeat(player.getLocation().toVector().subtract(new Vector(1,1,1)), 2, 15, true, player.getWorld(), this);
+        //DisplayUtil.showVectorRepeat(new Vector(4, 2, 4), player.getLocation().toVector(), 10, Color.PURPLE, true, player.getWorld(), this);
+        DisplayUtil.showVectorRepeat(player.getLocation().getDirection(), player.getEyeLocation().toVector(), 10, Color.ORANGE, true, player.getWorld(), this);
+        DisplayUtil.showVectorRepeat(new Vector(1,1,1), player.getLocation().toVector().subtract(new Vector(1,1,1)), 10, Color.PURPLE, true, player.getWorld(), this);
     }
 }
